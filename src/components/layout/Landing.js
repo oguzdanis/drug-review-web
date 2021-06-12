f933c6e945bce1df8869b6effe678de8166262f2
Oğuz Danış <oguzdanis@Oguzs-MacBook-Air.local>
2021-06-12 21:34:53 +0300

    initial commit

---
+++ src/components/layout/Landing.js
@@ -0,0 +1,21 @@
+const Landing = () => {
+    return (
+        <section className="landing">
+            <div className="dark-overlay">
+                <div className="landing-inner">
+                    <h1 className="x-large">İlaçlar hakkında bilgilenmeye hazırmısınız</h1>
+                    <p className="lead">
+                        Merak ettiğiniz tüm ilaçlarla ilgili uzman ve kullanıcı yorumları burada.
+                        Şimdi Üye ol.
+                    </p>
+                    <div className="buttons">
+                        <a href="register" class="btn btn-primary">Üye ol</a>
+                        <a href="login" class="btn btn-light">Giriş Yap</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Landing
---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar bg-dark">
+            <h1>
+                <a href="/"><i className="fas fa-code"></i> İlaç Yorumlama</a>
+            </h1>
+            <ul>
+                <li><Link to="/register">Üye ol</Link></li>
+                <li><Link to="/login">Giriş Yap</Link></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
